74ab34685ff8250ab712cd766f226f1cc3024d8a
Leonardo Aguilera Blanco <laguileraab@gmail.com>
2022-06-09 21:52:47 -0400

    Addded more unit tests

--- src/test/java/com/msoft/gateways/unittest/GatewayTests.java
+++ src/test/java/com/msoft/gateways/unittest/GatewayTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.msoft.gateways.dto.GatewayDTO;
+import com.msoft.gateways.dto.PeripheralDTO;
 import com.msoft.gateways.model.Gateway;
 import com.msoft.gateways.model.Peripheral;
 import com.msoft.gateways.repository.PeripheralRepository;
@@ -38,7 +40,7 @@ public class GatewayTests {
     void setup() {
         peripherals.add(Peripheral.builder()
         .online(true)
-        .vendor("Huawei")
+        .vendor("peripheral for test")
         .build());
         gateway = Gateway.builder()
             .name("gateway for test")
@@ -67,7 +69,30 @@ public class GatewayTests {
                 .build();
 
         GatewayDTO gatewayDTOU = gatewayService.updateGateway(gatewayU.getId().toString(), gatewayU);
-        assertNotEquals(gatewayDTOU, gatewayU);
         assertNotEquals(gatewayDTO, gatewayDTOU);
     }
+
+    @Test
+    void gatewayPeripheralTests() {
+        Peripheral peripheral = Peripheral.builder()
+        .online(true)
+        .vendor("peripheral for test attached")
+        .build();
+
+        Gateway gatewayU = Gateway.builder()
+        .id(gatewayDTO.getId())
+        .name("gateway for test updated")
+        .ipAddress("192.192.192.192")
+        .peripherals(peripherals)
+        .build();
+
+        GatewayDTO gatewayDTOU = gatewayService.addPeripheralToGateway(gatewayU.getId().toString(), peripheral);
+        assertEquals(peripherals.size() + 1, gatewayDTOU.getPeripherals().size());
+        
+        PeripheralDTO peripheralDTO = gatewayDTOU.getPeripherals().iterator().next();
+        gatewayService.removePeripheralFromGateway(gatewayDTOU.getId().toString(), peripheralDTO.getId());
+        GatewayDTO gatewayDTO = gatewayService.findGatewayById(gatewayDTOU.getId().toString());
+        assertEquals(gatewayDTO.getPeripherals().size(), gatewayDTOU.getPeripherals().size() - 1);
+    }
+
 }
